e51456724053b190253afbdf1a4554c86094ba45
jerome <jerome@core.com>
2017-07-25 11:08:41 +0800

    srpingboot demo

---
+++ src/main/java/com/example/demo/Hello.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+/**
+ * Created by Administrator on 2017/7/20.
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.print("xin ban ben");
+    }
+
+}
+
+
